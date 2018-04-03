d0b5a3f49b73dbf326b227583fddce8009478fc8
Rafael Krbashian <krbashianrafael@gmail.com>
2018-04-03 19:43:49 +0300

    работа над Treatment

--- app/src/main/java/com/gmail/krbashianrafael/medpunkt/TreatmentActivity.java
+++ app/src/main/java/com/gmail/krbashianrafael/medpunkt/TreatmentActivity.java
@@ -8,8 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.util.Linkify;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,24 +29,65 @@ public class TreatmentActivity extends AppCompatActivity {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_36dp);
         }
 
-        final EditText editText1 = findViewById(R.id.EditText1);
+        final TextView textViewAddPhoto1 = findViewById(R.id.textViewAddPhoto1);
+        final TextView textViewAddPhoto2 = findViewById(R.id.textViewAddPhoto2);
 
+        final EditText editText1 = findViewById(R.id.EditText1);
         final EditText editText2 = findViewById(R.id.EditText2);
+
+        editText1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus){
+                    textViewAddPhoto1.setVisibility(View.VISIBLE);
+                }
+                else {
+                    textViewAddPhoto1.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        editText2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus){
+                    textViewAddPhoto2.setVisibility(View.VISIBLE);
+                }
+                else {
+                    textViewAddPhoto2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        int unicode = 0x1F4F7;
+        final String unicodeString = new String(Character.toChars(unicode));
+
+        //editText1.setText(unicodeString);
+        editText1.setText(unicodeString + " Photo");
+        /*Drawable drawable = (ColorDrawable)editText1.getBackground();
+        editText1.getBackground().setColorFilter(drawable.getColor(), PorterDuff.Mode.SRC_IN);*/
+        //editText1.setText(menuIconWithText(getResources().getDrawable(R.drawable.ic_delete_red_24dp), getResources().getString(R.string.delete_photo)));
+
 
         final SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         final SharedPreferences.Editor prefsEditor = prefs.edit();
 
-        Button buttonToLink = findViewById(R.id.buttonToLink);
 
-        buttonToLink.setOnClickListener(new View.OnClickListener() {
+
+        textViewAddPhoto1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                textViewAddPhoto1.setVisibility(View.INVISIBLE);
+                editText2.setVisibility(View.VISIBLE);
+                editText2.requestFocus();
+
                 prefsEditor.putString("testText", editText1.getText().toString());
                 prefsEditor.apply();
 
                 editText2.setText(prefs.getString("testText","yyy"));
 
-                Pattern p = Pattern.compile("\\b_._ [A-Za-z0-9]*\\b");
+                //Pattern p = Pattern.compile("\\b" + unicode + "[A-Za-z0-9]*\\b");
+                Pattern p = Pattern.compile(unicodeString+ " [A-Za-z0-9]*\\b");
 
                 Linkify.TransformFilter dataTransformer = new Linkify.TransformFilter() {
                     @Override
